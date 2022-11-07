7cba2c09b0684e7ae276fa4452e056fc02fc17d6
phatltpb <phat718920@gmail.com>
2022-11-07 13:36:56 +0700

    fix port

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"gitlab.com/meta-node/mail/config"
 	"gitlab.com/meta-node/mail/core/database"
 	"gitlab.com/meta-node/mail/core/routers"
 	"gitlab.com/meta-node/mail/server"
@@ -19,6 +18,6 @@ func runHttpsServer() {
 	dbConn := database.InstanceDB()
 	database.Migration(dbConn)
 	r := routers.InitRouter()
-	port := config.GetConfig().Server.HTTP.Port
-	r.Run(port)
+	// port := config.GetConfig().Server.HTTP.Port
+	r.Run(":5000")
 }
